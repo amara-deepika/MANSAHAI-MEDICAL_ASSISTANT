bfcb1a90986497d54daf1a21197b669dae54eeb1
Amara Deepika <113845145+amara-deepika@users.noreply.github.com>
2023-10-09 17:17:00 +0530

    Update Mysqlconnector.py

--- Mysqlconnector.py
+++ Mysqlconnector.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="Omganapati@1",database="mansahaidatabase")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="XXXXXXXXXi@1",database="mansahaidatabase")
 mycursor=mydb.cursor()
 mycursor.execute("select * from mansahai")
 d=mycursor.fetchall()
